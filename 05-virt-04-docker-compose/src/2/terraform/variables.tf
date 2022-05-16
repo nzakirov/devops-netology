97aa8d2200e56ce7a467fe4b4e25f16e37fc2fc2
Nail Zakirov <nzakirov@gmail.com>
2022-05-16 10:11:01 +0300

    [05-virt-04-docker-compose] add 2 playbook

--- 05-virt-04-docker-compose/src/2/terraform/variables.tf
+++ 05-virt-04-docker-compose/src/2/terraform/variables.tf
@@ -13,5 +13,5 @@ variable "yandex_folder_id" {
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd8gngc849so4rdrnpns"
+  default = "fd86sli00a0s2qto4hk6"
 }
