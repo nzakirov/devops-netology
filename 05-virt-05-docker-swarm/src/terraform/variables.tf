bedca5d644def9a73934aab57498e5ac4d4efeed
Nail <nzakirov@gmail.com>
2022-05-24 20:38:59 +0300

    [05-virt-05-docker-swarm] add variables

--- 05-virt-05-docker-swarm/src/terraform/variables.tf
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1gdkem0mim4so5c0stp"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1gnrqrhvutnaq3vv08q"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd813d8inju2otjn8b2c"
 }
